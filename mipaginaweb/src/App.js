ce05cb1aa5b348a235ade940b48b80091a1cb145
Guillermo Garcia Torres <107325230+Memo611@users.noreply.github.com>
2024-10-03 17:16:16 -0700

    Navbar Mejorada y avance general

--- mipaginaweb/src/App.js
+++ mipaginaweb/src/App.js
@@ -1,27 +1,62 @@
 import './App.css';
 import { Navbar } from './Components/Navbar.js';
+import logo from './Constants/logo.svg'
 
 
 
 function App() {
   return (
     <div className="App">
-    <header className="App-header">
-      
-    <Navbar/>
+      <header className="App-header">
+        <Navbar />
 
-    </header>
-    <div className='App-body'>
-      <h1>
-        Holaaa
-      </h1>
-      <h1>
-        Holaaa
-      </h1><h1>
-        Holaaa
-      </h1>
+      </header>
+      <div className='App-body'>
+
+        <h1>Bienvenido a mi pagina web</h1>
+        <p>Soy Guillermo Garcia Desarrollador Web</p>
+
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+          <div class="col">
+            <div class="card">
+              <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
-  </div>
   );
 }
 
--- mipaginaweb/src/Components/Navbar.js
+++ mipaginaweb/src/Components/Navbar.js
@@ -1,37 +1,40 @@
 import React from 'react'
 import logo from '../Constants/logo.svg'
 export const Navbar = () => {
-    let nombre = "Memo"
-    let web = "Memogtweb.es"
 
     return (
-        <nav class="navbar" data-bs-theme="dark">
+        <nav class="navbar navbar-expand-lg w-100" data-bs-theme="dark">
+            <a class="navbar-brand" href="#">
+                <img src={logo} alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
+                Memo.web
+            </a>
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src={logo} alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
-                    Memo.web
-                </a>
-                <ul class="nav nav-underline">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">about me</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Projects</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Educations
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Skills
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact
-                        </a>
-                    </li>
-                    
-                </ul>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+                    <ul class="nav nav-underline">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Me</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Projects</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Educations
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Skills
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
             </div>
         </nav>
     )
