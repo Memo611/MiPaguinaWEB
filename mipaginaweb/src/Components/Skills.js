6b6a67aadc74cd2de387a25224d784b500b3946d
Guillermo Garcia Torres <107325230+Memo611@users.noreply.github.com>
2024-10-04 18:51:07 -0700

    cards skills base

--- mipaginaweb/src/Components/Skills.js
+++ mipaginaweb/src/Components/Skills.js
@@ -1,9 +1,71 @@
 import React from 'react'
-
+import logo from '../Constants/logo.svg';
+import '../Styles/Skills.css';
 export const Skills = () => {
   return (
     <div>
       <h1>Skills</h1>
+
+      {/* Diseño de cards para skills */}
+
+      <div class="row row-cols-md-7 custom-grid-skills">
+        <div class="col">
+          <div class="card custom-card-skills">
+            <img src={logo} class="card-img-top-skills" alt="..." />
+            <div class="card-body">
+
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card custom-card-skills">
+            <img src={logo} class="card-img-top-skills" alt="..." />
+            <div class="card-body">
+
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card custom-card-skills">
+            <img src={logo} class="card-img-top-skills" alt="..." />
+            <div class="card-body">
+
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card custom-card-skills">
+            <img src={logo} class="card-img-top-skills" alt="..." />
+            <div class="card-body">
+
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card custom-card-skills">
+            <img src={logo} class="card-img-top-skills" alt="..." />
+            <div class="card-body">
+
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card custom-card-skills">
+            <img src={logo} class="card-img-top-skills" alt="..." />
+            <div class="card-body">
+
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card custom-card-skills">
+            <img src={logo} class="card-img-top-skills" alt="..." />
+            <div class="card-body">
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
